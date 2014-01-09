f44ec0383a24192d0223b28e703271c5ee282c94
cnsworder <cnsworder@gmail.com>
2014-01-09 16:32:26 +0800

    添加初始化脚本

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s `which zsh`
+if [[ `id -u` == 0]]; then
+    echo "export EDITOR=vim" >> /etc/profile
+fi
