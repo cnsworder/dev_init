8a874f265bb0d74413baa1a008bc5f23573f9cca
cnsworder <cnsworder@gmail.com>
2014-01-09 16:36:47 +0800

    修改说明

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 配置文件
 =========
-#配置方法
-    git clone git@github.com:cnsworder/crossword  
-    ln -s crossword/emacs ~/.emacs  
-    ln -s crossword/vimrc ~/.vimrc  
 
-#说明
+配置方法
+--------    
+    ```
+       git clone git@github.com:cnsworder/crossword  
+       ./init.sh
+    ```
+   
+
+说明
+-----
+
   vim使用vundle进行包管理  
   emacs使用elpa进行包管理 
   配置说明:[cnsworder Blog] [1]   
--- init.sh
+++ init.sh
@@ -3,7 +3,10 @@
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-chsh -s `which zsh`
-if [[ `id -u` == 0]]; then
-    echo "export EDITOR=vim" >> /etc/profile
-fi
+chsh `which zsh`
+#if [[ `id -u` == 0 ]]; then
+#    echo "export EDITOR=vim" >> /etc/profile
+#fi
+
+ln -s ./crossword/emacs ~/.emacs 
+ln -s ./crossword/vimrc ~/.vimrc
