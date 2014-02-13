62a472a289270761e9a4fe0da108299ce4a3deb0
cnsworder <cnsworder@gmail.com>
2014-02-13 22:38:53 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-OS=$(cat /etc/issue)
-if [ "$OS" == "archlinu" ];then
+if [ -x /usr/bin/pacman ];then
     PM=${PM:=pacman}
     PM_INSTALL=-S
-else if [ "$OS" == "debian" || "$OS" == "redhat" ];then
+    OS=arch
+else if [ -x /usr/bin/apt-get ];then
     PM=apt-get
     PM_INSTALL=install
+    OS=debian
+else if [ -x /usr/bin/rpm ]; then
+    PM=rpm
+    PM_INSTALL=install
+    OS=redhat
 fi
+
+echo "OS: $OS"
+echo "using Package Manager $PM"
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
