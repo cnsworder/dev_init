4d7604e0ee1c4d578d51b5f1220971c50fa62284
cnsworder <cnsworder@gmail.com>
2014-02-13 22:33:53 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+OS=$(cat /etc/issue)
+if [ "$OS" == "archlinu" ];then
+    PM=${PM:=pacman}
+    PM_INSTALL=-S
+else if [ "$OS" == "debian" || "$OS" == "redhat" ];then
+    PM=apt-get
+    PM_INSTALL=install
+fi
+
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
@@ -10,3 +19,5 @@ chsh `which zsh`
 
 ln -s ./crossword/emacs ~/.emacs 
 ln -s ./crossword/vimrc ~/.vimrc
+
+${PM} ${PM_INSTALL}  
