606eb47333bdde6f363d658ddab80c371b46cc68
cnsworder <cnsworder@gmail.com>
2014-05-04 18:07:04 +0800

    先升级后在安装，同时增加X11的安装包

--- init.sh
+++ init.sh
@@ -1,32 +1,40 @@
 #!/usr/bin/env bash
 # author: cnsworder
 # email: cnsworder@gmail.com
-# date: 2014--2-14
+# date: 2014-02-14
+# version: 1.1
+
+PACKAGES=vim emacs tmux python ctags cscope curl aria2 zsh
+XPACKAGES=gvim
+PIP=pip
+PYTHON_PACKAGES=virtualenv
 
 if [ -x /usr/bin/pacman ];then
     PM=${PM:=pacman}
     PM_INSTALL=-S
     OS=arch
+    yes | pacman -Syu
 elif [ -x /usr/bin/apt-get ];then
     PM=apt-get
     PM_INSTALL=install
     OS=debian
+    yes | apt-get update; apt-get upgrade
 elif [ -x /usr/bin/yum ]; then
     PM=yum
     PM_INSTALL=install
     OS=redhat
+    yes | yum update
 elif [ -x /usr/bin/emerge ]; then
     PM=emerge
     PM_INSTALL=
     OS=gentoo
+    emerge world
 fi
 
 echo "OS: $OS"
-echo "using Package Manager $PM"
+echo "Package Manager: $PM"
 
 echo "Installing packages..."
-
-PACKAGES=vim emacs tmux python ctags cscope curl aria2 zsh zeal-git
 
 yes | ${PM} ${PM_INSTALL} ${PACKAGES}
 
@@ -43,6 +51,10 @@ chsh `which zsh`
 ln -s ./emacs ~/.emacs 
 ln -s ./vimrc ~/.vimrc
 
+
+# python
+echo "Update python environment..."
+
 if [ ! -d ~/.pip ]; then
     mkdir ~/.pip
 elif [ -e ~/.pip/pip.conf ]; then
@@ -50,5 +62,14 @@ elif [ -e ~/.pip/pip.conf ]; then
 fi
 
 ln -s ./pip.conf ~/.pip/pip.conf
+
+${PIP} install ${PYTHON_PACKAGES}
+
+if [[ $# == 1 ]]; then
+   if [[ "$1" = "gui" ]]; then 
+      echo "Install X Appliaction..."
+      yes | ${PM} ${PM_INSTALL} ${XPACKAGES}
+   fi
+fi
 
 read endf -p "Init finish..."
