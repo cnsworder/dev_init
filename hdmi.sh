1b24c2dea2bfc65774b468b8c02be94f18185b0a
cnsworder <cnsworder@gmail.com>
2014-12-01 09:38:30 +0800

    Add xrandr change two screen

---
+++ hdmi.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xrandr --output HDMI-0 --mode 1600x900 --rotate right --left-of LVDS-0
---
+++ vga.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xrandr --output VGA-0 --mode 800x600  --left-of LVDS-0
