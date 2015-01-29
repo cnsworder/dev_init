e1ba4439835a6c759031eeb476187657e8692d7b
cnsworder <cnsworder@gmail.com>
2015-01-29 08:58:42 +0800

    Update start i3wm exec hdmi output

--- hdmi.sh
+++ hdmi.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-xrandr --output HDMI-0 --mode 1600x900 --rotate right --left-of LVDS-0
+OUTPUT=$(xrandr | grep "HDMI-.*" | awk '{print $1}')
+
+echo "$output to: ${OUTPUT}"
+
+if [[ x"${OUTPUT:0:4}" == x"HDMI" ]]; then
+    echo "xrandr change..."
+    xrandr --output ${OUTPUT}  --mode 1600x900 --rotate right --left-of LVDS-0
+fi
