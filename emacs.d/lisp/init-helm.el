7e3bff627d9bd33b62b7618a9f10ea076042fc5e
crossorbit <cnsworder@gamil.com>
2016-05-09 13:59:45 +0800

    used swiper/ivy replace helm

--- emacs.d/lisp/init-helm.el
+++ emacs.d/lisp/init-helm.el
@@ -1,11 +1,11 @@
 
 ;;helm配置
-(require 'helm-config)
-(helm-mode 1)
-(setq helm-quick-update t)
-(setq helm-M-x-requires-pattern nil)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+;;(require 'helm-config)
+;;(helm-mode 1)
+;;(setq helm-quick-update t)
+;;(setq helm-M-x-requires-pattern nil)
+;;(global-set-key (kbd "M-x") 'helm-M-x)
+;;(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 
 ;; helm 替换smex和ido
 ;;(require 'smex)
--- emacs.d/lisp/keyset.el
+++ emacs.d/lisp/keyset.el
@@ -10,6 +10,18 @@
    (global-unset-key (kbd "C-SPC"))
    
    ;;快捷键设置
+   (global-set-key (kbd "M-x") 'counsel-M-x)
+   (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+   (global-set-key (kbd "C-s") 'swiper)
+   (global-set-key (kbd "C-c r") 'ivy-resume)
+   (global-set-key (kbd "C-c i") 'counsel-imenu)
+   (global-set-key (kbd "C-c g") 'counsel-git)
+   (global-set-key (kbd "C-c k") 'counsel-ag)
+   (global-set-key (kbd "C-c g") 'counsel-git)
+   (global-set-key (kbd "C-c v") 'counsel-describe-variable)
+   (global-set-key (kbd "C-c f") 'counsel-describe-function)   
+
    (global-set-key (kbd "<f3>") 'sr-speedbar-toggle)
    (global-set-key (kbd "<f4>") 'eshell)
    ;;(global-set-key (kbd "<f5>") '(shell-command '"astyle --style=kr"))
@@ -19,14 +31,10 @@
    (if (eq system-type 'darwin)
        (global-set-key (kbd "C-c d") 'dash-at-point)
        (global-set-key (kbd "C-c d") 'zeal-at-point))
-   (global-set-key (kbd "C-c b") 'refill-mode)
+   ;;(global-set-key (kbd "C-c b") 'refill-mode)
+   
    (global-set-key (kbd "C-c c") 'c++-mode)
-   (global-set-key (kbd "C-c i") 'helm-imenu)
-   (global-set-key (kbd "C-s") 'swiper)
-   (global-set-key (kbd "C-c r") 'ivy-resume)
    (global-set-key (kbd "C-x C-r") 'recentf-open-files)
-   (global-set-key (kbd "C-c g") 'counsel-git)
-   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
    )
 
 (key-set)
