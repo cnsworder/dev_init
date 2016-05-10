c22bfee6d25f2679c69c72f9f97a3403a8b1d541
crossorbit <cnsworder@gamil.com>
2016-05-10 12:13:53 +0800

    check mac os

--- emacs.d/lisp/keyset.el
+++ emacs.d/lisp/keyset.el
@@ -7,9 +7,11 @@
    (interactive)
 
    ;; meta键调整
-   (setq mac-command-modifier 'meta)
-   (setq mac-control-modifier 'control)
-   (setq mac-option-modifier 'alt)
+   (if (eq system-type 'darwin)
+       (progn
+        (setq mac-command-modifier 'meta)
+        (setq mac-control-modifier 'control)
+        (setq mac-option-modifier 'alt)))
    
    ;;避免输入法切换冲突
    (global-unset-key (kbd "C-SPC"))
