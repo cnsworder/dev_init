ae6a748e9598e880025902951d5cfbe616e831cb
crossorbit <cnsworder@gamil.com>
2016-06-14 12:50:24 +0800

    Update hook company

--- emacs.d/lisp/init-dev.el
+++ emacs.d/lisp/init-dev.el
@@ -62,14 +62,14 @@
 ;;(add-hook 'c++-mode-hook 'ycmd-mode)
 (company-ycmd-setup)
 
-(add-hook 'emacs-lisp-hook
-          (lambda()
-          (setq (make-local-variable 'company-backends)
-                '(company-elisp
-                  company-yasnippet
-                  company-eclim
-                  company-abbrev
-                  company-dabbrev))))
+;; (add-hook 'emacs-lisp-hook
+;;          (lambda()
+;;          (setq (make-local-variable 'company-backends)
+;;                '(company-elisp
+;;                  company-yasnippet
+;;                  company-eclim
+;;                  company-abbrev
+;;                  company-dabbrev))))
 
 (require 'company-go)
 (add-hook 'go-mode-hook
