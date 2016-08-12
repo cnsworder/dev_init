b2c1aa49ed9733912c304513784bb84c6c0471cb
crossorbit <cnsworder@gamil.com>
2016-08-12 16:49:21 +0800

    Update

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 PACKAGES="tmux python stow vim emacs ctags cscope global curl aria2 tig ranger the_silver_searcher autojump"
 DEV_PACKAGES="gcc-c++ clang zeal"
 MANAGES="fabric ansible"
-XPACKAGES="gvim i3 termite rofi compton"
+XPACKAGES="i3 termite rofi compton gvim"
 PIP="pip"
-PYTHON_PACKAGES="virtualenv"
+PYTHON_PACKAGES="virtualenv ipython"
