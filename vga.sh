4723738698d2db7e5e57b25d8742823959f81815
root <root@localhost.localdomain>
2016-08-17 09:41:05 +0800

    Update vga auto

--- vga.sh
+++ vga.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-xrandr --output VGA-0 --mode 800x600  --left-of LVDS-0
+arg=$1
+
+if [[ $arg == '' ]]; then
+    xrandr --output VGA-0 --mode 800x600  --left-of LVDS-0
+else
+    xrandr --output VGA-1 --auto
+fi
