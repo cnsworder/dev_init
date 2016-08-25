fa100c97ba206d47e5275414dc59afd356be6a13
root <root@localhost.localdomain>
2016-08-25 13:23:52 +0800

    Update packeage

--- package.sh
+++ package.sh
@@ -1,6 +1,27 @@
-PACKAGES="tmux python stow vim emacs ctags cscope global curl aria2 tig ranger the_silver_searcher autojump"
-DEV_PACKAGES="gcc-c++ clang zeal"
-MANAGES="fabric ansible"
-XPACKAGES="i3 termite rofi compton gvim"
+PACKAGES="tmux 
+          python 
+          stow
+          vim
+          emacs
+          ctags
+          cscope
+          global
+          curl
+          aria2
+          tig
+          ranger
+          the_silver_searcher
+          autojump"
+DEV_PACKAGES="gcc-c++
+              clang
+              zeal"
+MANAGES="fabric
+         ansible"
+XPACKAGES="i3 
+           termite
+           rofi
+           compton
+           gvim"
 PIP="pip"
-PYTHON_PACKAGES="virtualenv ipython"
+PYTHON_PACKAGES="virtualenv
+                 ipython"
