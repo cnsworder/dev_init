9631646798f3d0f936a0a2cf6cb1fdc74e4dd152
cnsworder <cnsworder@gmail.com>
2017-06-28 08:47:12 +0800

    Add dumb-jump

--- emacs.d/lisp/custom.el
+++ emacs.d/lisp/custom.el
@@ -9,11 +9,11 @@
          ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
     '(custom-safe-themes
          (quote
-             ("b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "d320493111089afba1563bc3962d8ea1117dd2b3abb189aeebdc8c51b5517ddb" default)))
+             ("d29231b2550e0d30b7d0d7fc54a7fb2aa7f47d1b110ee625c1a56b30fea3be0f" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "d320493111089afba1563bc3962d8ea1117dd2b3abb189aeebdc8c51b5517ddb" default)))
     '(nil nil t)
     '(package-selected-packages
          (quote
-             (tern add-node-modules-path symbol-overlay vue-mode autothemer company flycheck git-commit request cask-mode use-package gruvbox-theme dashboard counsel-gtags fzf py-autopep8 counsel counsel-osx-app counsel-projectile swiper crosshairs bm init-open-recentf mode-icons all-the-icons xcscope window-numbering which-key web-mode tabbar super-save smartparens popwin popup neotree molokai-theme markdown-mode magit js2-mode indent-guide iedit highlight-parentheses google-c-style git-gutter ggtags flycheck-ycmd flycheck-google-cpplint expand-region exec-path-from-shell evil emmet-mode elpy editorconfig ecb dracula-theme direx dired+ dash-at-point crux company-ycmd company-web company-quickhelp company-go company-c-headers command-log-mode cmake-mode avy anaconda-mode aggressive-indent ag ace-jump-mode cask zeal-at-point undo-tree projectile pallet proceed flycheck-cask))))
+             (dumb-jump tern add-node-modules-path symbol-overlay vue-mode autothemer company flycheck git-commit request cask-mode use-package gruvbox-theme dashboard counsel-gtags fzf py-autopep8 counsel counsel-osx-app counsel-projectile swiper crosshairs bm init-open-recentf mode-icons all-the-icons xcscope window-numbering which-key web-mode tabbar super-save smartparens popwin popup neotree molokai-theme markdown-mode magit js2-mode indent-guide iedit highlight-parentheses google-c-style git-gutter ggtags flycheck-ycmd flycheck-google-cpplint expand-region exec-path-from-shell evil emmet-mode elpy editorconfig ecb dracula-theme direx dired+ dash-at-point crux company-ycmd company-web company-quickhelp company-go company-c-headers command-log-mode cmake-mode avy anaconda-mode aggressive-indent ag ace-jump-mode cask zeal-at-point undo-tree projectile pallet proceed flycheck-cask))))
 (custom-set-faces
     ;; custom-set-faces was added by Custom.
     ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs.d/lisp/init-dev.el
+++ emacs.d/lisp/init-dev.el
@@ -9,6 +9,17 @@
     :config
     (yas-global-mode t))
 
+(use-package dumb-jump
+    :bind
+    (("M-g o" . dumb-jump-go-other-window)
+        ("M-g j" . dumb-jump-go)
+        ("M-g i" . dumb-jump-go-prompt)
+        ("M-g x" . dumb-jump-go-prefer-external)
+        ("M-g z" . dumb-jump-go-prefer-external-other-window))
+    :config
+    (dumb-jump-mode)
+    (setq dumb-jump-selector 'ivy))
+
 ;; editorconfig
 (use-package editorconfig
     :config
