c29991607f29f08fb079eecfceb1473c28593048
cnsworder <cnsworder@gmail.com>
2018-06-30 11:07:55 +0800

    Updat install package

--- package.sh
+++ package.sh
@@ -1,20 +1,24 @@
 PACKAGES="zsh
           tmux
+          python3
+          mutt
           mosh
-          python
           stow
-          vim
-          emacs
-          ctags
-          cscope
-          global
-          curl
-          aria2
+          git
           tig
+          vim
+          aria2
+          curl
           ranger
           fzf
           lrzsz
+          htop
+          iftop
+          netcat
+          tcpdump
+          direnv
           the_silver_searcher
+          fasd
           autojump"
 
 
