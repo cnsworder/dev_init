157a6d60636439301584d61b5b291dc8e0829665
cnsworder <cnsworder@gmail.com>
2025-03-20 20:32:42 +0800

    Update:消除背景

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,3 +45,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq byte-compile-warnings '(cl-functions))
+(setq warning-suppress-log-types '((package reinitialization)))
