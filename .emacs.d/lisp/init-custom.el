c57bb95db0cbf559ae02f57998ab70c2afc62b01
cnsworder <cnsworder@gmail.com>
2025-10-02 12:11:20 +0800

    Update: 平滑滚动

--- .emacs.d/lisp/init-custom.el
+++ .emacs.d/lisp/init-custom.el
@@ -9,6 +9,14 @@
 (setq-default default-buffer-file-coding-system 'utf-8-unix)
 (prefer-coding-system 'utf-8-unix)
 
+(use-package ultra-scroll
+  :ensure t
+  :init
+  (setq scroll-conservatively 3 ; or whatever value you prefer, since v0.4
+        scroll-margin 0)        ; important: scroll-margin>0 not yet supported
+  :config
+  (ultra-scroll-mode 1))
+
 ;; 扩展选择区域
 (use-package expand-region
   :ensure t)
