90335fa8d94b26e5e313aa6e84639a57ead115f6
cnsworder <cnsworder@gmail.com>
2025-12-27 10:49:45 +0800

    Update: 优化daemon启动

--- .emacs.d/lisp/init-theme.el
+++ .emacs.d/lisp/init-theme.el
@@ -1,16 +1,6 @@
 ;;; packates init-theme
 ;;; Code:
 
-;; 去掉工具栏
-(if window-system
-    (tool-bar-mode 0))
-
-;; 开启折叠
-;; (hs-minor-mode t)
-;; 去掉滚动条
-;;(scroll-bar-mode 0)
-;;(fringe-mode 0)
-
 ;; 显示行号
 (global-display-line-numbers-mode t)
 (column-number-mode t)
@@ -53,10 +43,34 @@
 ;;(set-cursor-color "White")
 (setq-default cursor-type 'bar)
 
-;; 字体配置
-;;(set-default-font "SauceCodePro Nerd Font 16")
-(set-frame-font "-*-maple mono normal nf cn-medium-normal-*-16-*")
-(set-frame-font (font-spec :family "Maple Mono Normal NF CN" :size 16))
+(defun frame-ui (&optional frame)
+  (when (display-graphic-p)
+    ;; 去掉工具栏
+    (if window-system
+        (tool-bar-mode 0))
+    )
+
+  ;; 开启折叠
+  ;; (hs-minor-mode t)
+  ;; 去掉滚动条
+  ;;(scroll-bar-mode 0)
+  ;;(fringe-mode 0)
+
+  ;; 字体配置
+  ;;(set-default-font "SauceCodePro Nerd Font 16")
+  (set-frame-font "-*-maple mono normal nf cn-medium-normal-*-16-*")
+  (set-frame-font (font-spec :family "Maple Mono Normal NF CN" :size 16))
+  )
+
+(defun frame-ui-in-daemon (frame)
+  "Reload the theme (and font) in an daemon frame."
+  (when (or (daemonp) (not (display-graphic-p)))
+    (with-selected-frame frame
+      (run-with-timer 0.1 nil #'frame-ui))))
+
+(if (daemonp)
+    (add-hook 'after-make-frame-functions #'frame-ui-in-daemon)
+  (frame-ui))
 
 (provide 'init-theme)
 
